e17e469a9a1b5e62af85c1ee1a1f0e6cb4210f3f
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-02-19 16:23:13 +0530

    for cond

--- create-ec2.sh
+++ create-ec2.sh
@@ -3,5 +3,11 @@
 instances = ("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
 
 for name in ${instances[@]}; do
+    if [ $name == "shipping" ] || [ $name == "mysql" ]
+    then
+        instance_type = "t3.medium"
+    else
+        instance_type = "t3.micro"
+    fi
     echo "creating instance for: $name"
 done 
