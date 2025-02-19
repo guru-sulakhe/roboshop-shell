512de7c5a9cc3dde3964e42f080e1e0e1305bb1a
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-02-19 16:14:48 +0530

    for cond

--- create-ec2.sh
+++ create-ec2.sh
@@ -3,5 +3,5 @@
 instances = ("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
 
 for name in ${instances[@]}; do
-    echo "creating instance for: ${name}"
+    echo "creating instance for: $name"
 done 
