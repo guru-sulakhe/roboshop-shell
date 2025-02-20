7afb98c69dc2202eefbc6a48da84bd65d0d525ba
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-02-20 21:02:33 +0530

    scripting mysql

---
+++ 03-mysql.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+R = "\e[31m"
+G = "\e[32m"
+Y = "\e[33m"
+N = "\e[0m"
+
+VALIDATE(){
+    if [$1 -ne 0]
+    then
+        echo -e "$2.. $R Failed $N"
+    else
+        echo -e "$2.. $G Successful $N" 
+    fi   
+}
+
+if [$USERID -ne 0]
+then
+    echo "you don't have access, only root-user can access the file, try to login in as root-user"
+    exit 1 # manually exits if error
+else
+    echo "You are super-user"
+fi
+
+dnf install mysql-server -y &>> $LOGFILE
+VALIDATE $? "Installing mysql-server"
+
+systemctl enable mysqld &>> $LOGFILE
+VALIDATE $? "Enabling mysqld"
+
+systemctl start mysqld &>> $LOGFILE
+VALIDATE $? "Starting mysqld"
+
+mysql_secure_installation --set-root-pass RoboShop@1
+
+
+
+
+
+
