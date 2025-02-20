1e783345ef3c822a35350d74550479fe52e90e71
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-02-20 20:53:22 +0530

    configuring redis

---
+++ 02-redis.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+R = "\e[31m"
+G = "\e[32m"
+Y = "\e[33m"
+N = "\e[0m"
+
+VALIDATE(){
+    if [$1 -ne 0]
+    then
+        echo -e "$2.. $R Failed $N"
+    else
+        echo -e "$2.. $G Successful $N" 
+    fi   
+}
+
+if [$USERID -ne 0]
+then
+    echo "you don't have access, only root-user can access the file, try to login in as root-user"
+else
+    echo "You are super-user"
+fi
+
+dnf install redis -y &>> $LOGFILE
+VALIDATE $? "Installing Redis"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf &>> $LOGFILE
+VALIDATE $? "Remote Server Access"
+
+systemctl enable redis &>> $LOGFILE
+VALIDATE $? "Enabling Redis"
+
+systemctl start redis &>> $LOGFILE
+VALIDATE $? "Start Redis"
+
+
+
+
+
