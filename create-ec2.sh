97a0e5e44828e7df59ea6cd46943424fa78cfd4a
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-02-20 18:49:33 +0530

    adding tags,get private-ip from ec2-instances

--- create-ec2.sh
+++ create-ec2.sh
@@ -10,4 +10,9 @@ for name in ${instances[@]}; do
         instance_type="t3.micro"
     fi
     echo "creating instance for: $name with instance type: $instance_type"
+    instance_id=$(aws ec2 run-instances --image-id ami-454adgd12dd3dedbr --instance-type $instance_type --security-group-ids sg-903004f8oxg1sw4q --subnet-id subnet-6e7f829e4483287954 --query "Instances[0].InstanceId" --output text) #creating ec2 instance
+
+    aws ec2 create-tags --resource $instance_id --tags Key=Name, Value=$name #creating tags for var instance_id 
+    
+    private_ip=$(aws ec2 describe-instances --instance-ids i-04429f69b1bbd7867 --query 'Reservations[0].Instances[0].[PrivateIpAddress]' --output text) #describing ec2,for displaying IP address
 done 
