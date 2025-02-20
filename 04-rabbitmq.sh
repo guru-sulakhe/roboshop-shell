4474b1125f9d15e00f1384a19b1a3b76952b2e79
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-02-20 21:12:05 +0530

    scripting rabbitmq

---
+++ 04-rabbitmq.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+R = "\e[31m"
+G = "\e[32m"
+Y = "\e[33m"
+N = "\e[0m"
+
+VALIDATE(){
+    if [$1 -ne 0]
+    then
+        echo -e "$2.. $R Failed $N"
+    else
+        echo -e "$2.. $G Successful $N" 
+    fi   
+}
+
+if [$USERID -ne 0]
+then
+    echo "you don't have access, only root-user can access the file, try to login in as root-user"
+    exit 1 # manually exits if error
+else
+    echo "You are super-user"
+fi
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>> $LOGFILE
+VALIDATE $? "Configuring YUM Repos from the script provided by vendor"
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>> $LOGFILE
+VALIDATE $? "Configurring YUM Repos for RabbitMQ"
+
+dnf install rabbitmq-server -y  &>> $LOGFILE
+VALIDATE $? "Installing rabbitmq-server"
+
+systemctl enable rabbitmq-server  &>> $LOGFILE
+VALIDATE $? "Enabling rabbitmq-server"
+
+systemctl start rabbitmq-server &>> $LOGFILE
+VALIDATE $? "Starting rabbitmq-server"
+
+rabbitmqctl add_user roboshop roboshop123 &>> $LOGFILE
+VALIDATE $? "Adding user-roboshop and password"
+
+
+
+
