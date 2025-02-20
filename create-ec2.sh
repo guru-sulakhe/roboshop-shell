2d46c04f2fef65c04afc06c0c7826bc9739f925f
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-02-20 19:33:16 +0530

    creating R53 RECORDS for ec2-instances

--- create-ec2.sh
+++ create-ec2.sh
@@ -2,6 +2,7 @@
 
 instances=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
 domain_name="guru97s.cloud"
+hosted_zone_id="Z08844556711QEV7DH9F" #YOUR HOSTED ZONE ID
 
 for name in ${instances[@]}; do
     if [ $name == "shipping" ] || [ $name == "mysql" ]
@@ -18,11 +19,27 @@ for name in ${instances[@]}; do
     if [$name=="web"]
     then
         aws ec2 wait instance-running --instance-ids $instance_id #wating the instance, untill instance starts running,then only public-ip will be created
-        public_ip=$(aws ec2 describe-instances --instance-ids i-04429f69b1bbd7867 --query 'Reservations[0].Instances[0].[PublicIpAddress]' --output text) #describing ec2,for displaying public-IP address
+        public_ip=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].[PublicIpAddress]' --output text) #describing ec2,for displaying public-IP address
         ip_to_use=$public_ip
     else
-        private_ip=$(aws ec2 describe-instances --instance-ids i-04429f69b1bbd7867 --query 'Reservations[0].Instances[0].[PrivateIpAddress]' --output text) #describing ec2,for displaying private-IP address
+        private_ip=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].[PrivateIpAddress]' --output text) #describing ec2,for displaying private-IP address
         ip_to_use=$private_ip
     fi
 
-done 
+    #creating R53 record for ec2-instances
+    aws route53 change-resource-record-sets --hosted-zone-id $zoneid --change-batch '
+    {
+    "Comment": "Creating a record set for '$name'"
+    ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+        "Name"              : "'$name.$domain_name'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$ip_to_use'"
+        }]
+        }
+    }]
+    }'
+done  
