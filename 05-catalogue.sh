94042ac36050c8e7d416f79342b3c7ea37d27dbf
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-02-21 10:53:28 +0530

    scripting for catalogue

---
+++ 05-catalogue.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+R = "\e[31m"
+G = "\e[32m"
+Y = "\e[33m"
+N = "\e[0m"
+
+VALIDATE(){
+    if [$1 -ne 0]
+    then
+        echo -e "$2.. $R Failed $N"
+    else
+        echo -e "$2.. $G Successful $N" 
+    fi   
+}
+
+if [$USERID -ne 0]
+then
+    echo "you don't have access, only root-user can access the file, try to login in as root-user"
+    exit 1 # manually exits if error
+else
+    echo "You are super-user"
+fi
+
+dnf module disable nodejs -y &>> $LOGFILE
+VALIDATE $? "Disabling nodejs"
+
+dnf module enable nodejs:20 -y &>> $LOGFILE
+VALIDATE $? "Enabling nodejs"
+
+dnf install nodejs -y &>> $LOGFILE
+VALIDATE $? "Installing nodejs"
+
