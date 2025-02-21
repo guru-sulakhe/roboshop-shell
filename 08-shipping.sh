451fd2e6422777052417c30da0803ca812086de5
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-02-21 11:55:40 +0530

    scripting shipping

--- 08-shipping.sh
+++ 08-shipping.sh
@@ -4,6 +4,7 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPTNAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+MYSQL_HOST=mysql.guru97s.cloud
 R = "\e[31m"
 G = "\e[32m"
 Y = "\e[33m"
@@ -26,20 +27,47 @@ else
     echo "You are super-user"
 fi
 
-dnf install maven -y
+dnf install maven -y &>> $LOGFILE
+VALIDATE $? "installing java packages"
 
-useradd roboshop
+id roboshop &>> $LOGFILE
+if [ $? -ne 0 ]
+then
+    useradd roboshop &>> $LOGFILE
+    VALIDATE $? "Adding roboshop user"
+else
+    echo -e "roboshop user already exist...$Y SKIPPING $N"
+fi
 
-mkdir /app
+rm -rf /app &>> $LOGFILE
+VALIDATE $? "clean up existing directory"
 
-curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip
+mkdir /app &>> $LOGFILE
+VALIDATE $? "Changing to app directory"
 
-cd /app
+curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip &>> $LOGFILE
+VALIDATE $? "downloading shipping code and storing in tmp directory"
 
-unzip /tmp/shipping.zip
+cd /app &>> $LOGFILE
+VALIDATE $? "changing to app directory"
+
+unzip /tmp/shipping.zip &>> $LOGFILE
+VALIDATE $? "unziping shipping.zip"
 
 mvn clean package
 
 mv target/shipping-1.0.jar shipping.jar
 
 cp shipping /etc/systemd/system/shipping.service
+
+systemctl daemon-reload
+
+systemctl enable shipping 
+
+systemctl start shipping
+
+dnf install mysql -y
+
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e "use cities" &>> $LOGFILE
+
+systemctl restart shipping
