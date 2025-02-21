76ddfe22024c5c79c473fee1aac3615219ff5cf7
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-02-21 11:40:15 +0530

    adding shipping

--- 08-shipping.sh
+++ 08-shipping.sh
@@ -26,3 +26,20 @@ else
     echo "You are super-user"
 fi
 
+dnf install maven -y
+
+useradd roboshop
+
+mkdir /app
+
+curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip
+
+cd /app
+
+unzip /tmp/shipping.zip
+
+mvn clean package
+
+mv target/shipping-1.0.jar shipping.jar
+
+cp shipping /etc/systemd/system/shipping.service
