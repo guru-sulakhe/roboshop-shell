e101fc2e33bdc837c7f01cb87680399c0ed968a8
guru-sulakhe <sulakheguruprasad@gmail.com>
2025-02-21 12:32:22 +0530

    scripting web

---
+++ 10-web.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
+MYSQL_HOST=mysql.guru97s.cloud
+R = "\e[31m"
+G = "\e[32m"
+Y = "\e[33m"
+N = "\e[0m"
+
+VALIDATE(){
+    if [$1 -ne 0]
+    then
+        echo -e "$2.. $R Failed $N"
+    else
+        echo -e "$2.. $G Successful $N" 
+    fi   
+}
+
+if [$USERID -ne 0]
+then
+    echo "you don't have access, only root-user can access the file, try to login in as root-user"
+    exit 1 # manually exits if error
+else
+    echo "You are super-user"
+fi
+
+dnf install nginx -y &>> $LOGFILE
+VALIDATE $? "installing nginx"
+
+systemctl enable nginx &>> $LOGFILE
+VALIDATE $? "Enabling nginx"
+
+systemctl start nginx &>> $LOGFILE
+VALIDATE $? "Starting nginx"
+
+rm -rf /usr/share/nginx/html/* &>> $LOGFILE
+VALIDATE $? "Removing usr directory"
+
+curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>> $LOGFILE
+VALIDATE $? "downloading web.zip tmp directory"
+
+cd /usr/share/nginx/html &>> $LOGFILE
+VALIDATE $? "changing to usr directory"
+
+unzip /tmp/web.zip &>> $LOGFILE
+VALIDATE $? "unziping web.zip in tmp directory"
+
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf &>> $LOGFILE
+VALIDATE $? "copying roboshop.conf to etc directory"
+
+systemctl restart nginx &>> $LOGFILE
+VALIDATE $? "Restarting nginx service"
